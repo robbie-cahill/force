b6a7969066768b4b9c0a7cfcaa16d90101532b55
TanjieM <tanjie@democracy.works>
2022-09-26 12:18:54 -0500

    Revert "feat(ArtQuiz): main screen"

--- src/Apps/ArtQuiz/ArtQuizApp.tsx
+++ src/Apps/ArtQuiz/ArtQuizApp.tsx
@@ -1,10 +1,7 @@
-import { ArtQuizContextProvider } from "Apps/ArtQuiz/ArtQuizContext/ArtQuizContext"
-import { ArtQuizInterface } from "./ArtQuizInterface/ArtQuizInterface"
-
 export const ArtQuizApp = () => {
   return (
-    <ArtQuizContextProvider>
-      <ArtQuizInterface />
-    </ArtQuizContextProvider>
+    <div>
+      <h1>ArtQuiz</h1>
+    </div>
   )
 }
