8c5698ee02ff61846a3c4d0c827aa66a2a8b4faa
Dima Tretyak <itretyak@ya.ru>
2023-04-21 00:12:07 +0300

    sandbox

--- src/Components/Search/NewSearch/NewSearchInputPillsContainer.tsx
+++ src/Components/Search/NewSearch/NewSearchInputPillsContainer.tsx
@@ -5,8 +5,20 @@ import {
   ShelfNavigationProps,
   ShelfNext,
   ShelfPrevious,
+  compound,
+  visuallyDisableScrollbar,
 } from "@artsy/palette"
-import { useEffect, useRef, useState } from "react"
+import { ReactNode, useCallback } from "react"
+import {
+  Children,
+  FC,
+  createRef,
+  isValidElement,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react"
 import styled, { css } from "styled-components"
 
 const PILLS_OPTIONS = [
@@ -22,10 +34,15 @@ const PILLS_OPTIONS = [
   "GALLERY",
 ]
 
+// const GRADIENT_BG_WIDTH = 80
+const CHEVRON_BTN_SIZE = 30
+const CHEVRON_BTN_OFFSET = 20
+const CHEVRON_CONTAINER_WIDTH = CHEVRON_BTN_OFFSET + CHEVRON_BTN_SIZE
+
 const ChevronStyle = css`
   position: absolute;
-  width: 30px;
-  height: 30px;
+  width: ${CHEVRON_BTN_SIZE}px;
+  height: ${CHEVRON_BTN_SIZE}px;
   @media (hover: none) {
     display: none;
   }
@@ -39,165 +56,229 @@ const NextChevron = styled(ShelfNext)<ShelfNavigationProps>`
   ${ChevronStyle}
 `
 
-interface GradientBgProps {
-  placement: "right" | "left"
-}
+// interface GradientBgProps {
+//   placement: "right" | "left"
+// }
 
-const GradientBg = styled(Box)<GradientBgProps>`
-  position: "absolute";
-  right: 0;
-  width: 100px;
-  height: 30px;
-  background: transparent;
-  background-image: linear-gradient(
-    to ${props => props.placement},
-    rgba(255, 255, 255, 0) 0%,
-    rgba(255, 255, 255, 1) 40%
-  );
-`
+// const GradientBg = styled(Box)<GradientBgProps>`
+//   position: "absolute";
+//   right: 0;
+//   width: ${GRADIENT_BG_WIDTH}px;
+//   height: ${CHEVRON_BTN_SIZE}px;
+//   background: transparent;
+//   background-image: linear-gradient(
+//     to ${props => props.placement},
+//     rgba(255, 255, 255, 0) 0%,
+//     rgba(255, 255, 255, 1) 40%
+//   );
+// `
 
 const PillsContainer = styled(Flex)`
   flex-wrap: nowrap;
   overflow-x: auto;
   scrollbar-width: none;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
+  -webkit-overflow-scrolling: touch;
+  scroll-snap-type: x mandatory;
+  ${visuallyDisableScrollbar}
 `
 
-const GRADIENT_BG_WIDTH = 30
+const content = PILLS_OPTIONS.map((pill, index) => {
+  const isLastPill = index === PILLS_OPTIONS.length - 1
 
-export const NewSearchInputPillsContainer = () => {
-  const pillsRef = useRef<HTMLDivElement>(null)
+  return (
+    <Pill key={pill} mr={isLastPill ? 0 : 1}>
+      {pill}
+    </Pill>
+  )
+})
+
+export const NewSearchInputPillsContainer: FC = () => {
+  const pillsContainerRef = useRef<HTMLDivElement>(null)
+  const containerRef = useRef<HTMLDivElement>(null)
   const [showPreviousChevron, setShowPreviousChevron] = useState<boolean>(false)
   const [showNextChevron, setShowNextChevron] = useState<boolean>(false)
 
-  // On inital load
-  useEffect(() => {
-    if (pillsRef.current) {
-      const pillsContainer = pillsRef.current
-      setShowNextChevron(
-        pillsContainer.scrollWidth > pillsContainer.clientWidth
-      )
-    }
-  }, [])
+  const pills = useChildrenRefs(content)
+  const [offsets, setOffsets] = useState([0])
+  const [index, setIndex] = useState(0)
 
-  // when screen width changes
-  useEffect(() => {
-    const handleResize = () => {
-      if (pillsRef.current) {
-        const pillsContainer = pillsRef.current
-        if (pillsContainer.scrollWidth <= pillsContainer.clientWidth) {
-          setShowNextChevron(false)
-        } else {
-          setShowNextChevron(true)
-        }
-      }
+  // console.log("[debug] offsets", offsets)
+
+  const scrollTo = (position: number) => {
+    const pillsContainer = pillsContainerRef.current
+
+    if (!pillsContainer) {
+      return
     }
 
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
-  }, [])
-
-  const scrollToLeft = () => {
-    if (pillsRef.current) {
-      const pillsContainer = pillsRef.current
-      const pills = pillsContainer.children
-      const currentScroll = pillsContainer.scrollLeft
-      const visibleWidth = pillsContainer.offsetWidth
-      let scrollBy = 0
-
-      // Find the last visible pill and the next pill
-      for (let i = pills.length - 1; i >= 0; i--) {
-        const pill = pills[i] as HTMLElement
-        if (
-          pill.offsetLeft + pill.offsetWidth <=
-          currentScroll + visibleWidth
-        ) {
-          const prevPill = pills[i - 1] as HTMLElement
-          scrollBy = -prevPill.offsetWidth
-          break
-        }
-      }
-
-      pillsContainer.scrollBy({
-        left: scrollBy - GRADIENT_BG_WIDTH,
+    if (pillsContainer.scrollTo) {
+      pillsContainer.scrollTo({
+        left: position,
         behavior: "smooth",
       })
+
+      return
     }
+
+    pillsContainer.scrollLeft = position
+  }
+
+  const scrollToIndex = (offsetIndex: number) => {
+    const position = offsets[offsetIndex]
+    scrollTo(position)
+  }
+
+  const scrollToLeft = () => {
+    if (index === 0) {
+      scrollTo(0)
+      return
+    }
+
+    scrollToIndex(index - 1)
   }
 
   const scrollToRight = () => {
-    if (pillsRef.current) {
-      const pillsContainer = pillsRef.current
-      const pills = pillsContainer.children
-      const currentScroll = pillsContainer.scrollLeft
-      let scrollBy = 0
-
-      // Find the first visible pill and the next pill
-      for (let i = 0; i < pills.length; i++) {
-        const pill = pills[i] as HTMLElement
-        if (pill.offsetLeft >= currentScroll) {
-          const nextPill = pills[i + 1] as HTMLElement
-          scrollBy = nextPill.offsetWidth
-          break
-        }
-      }
-
-      pillsContainer.scrollBy({
-        left: scrollBy + GRADIENT_BG_WIDTH,
-        behavior: "smooth",
-      })
-    }
+    scrollToIndex(index + 1)
   }
 
-  const handleScroll = () => {
-    if (pillsRef.current) {
-      const currentPosition = pillsRef.current.scrollLeft
-      const maxScroll =
-        pillsRef.current.scrollWidth - pillsRef.current.clientWidth
+  const handleScroll = useCallback(() => {
+    const pillsContainer = pillsContainerRef.current
 
-      // Check if the scroll position is at the beginning or end
-      if (currentPosition === 0) {
-        // Hide PreviousChevron when pills is at the beginning
-        setShowPreviousChevron(false)
-        setShowNextChevron(true)
-      } else if (currentPosition === maxScroll) {
-        // Hide NextChevron when pills reaches the end
-        setShowPreviousChevron(true)
-        setShowNextChevron(false)
-      } else {
-        // Show both chevrons otherwise
-        setShowPreviousChevron(true)
-        setShowNextChevron(true)
-      }
+    if (!pillsContainer) {
+      return
     }
-  }
+
+    const currentScroll = pillsContainer.scrollLeft
+    const maxScroll = pillsContainer.scrollWidth - pillsContainer.clientWidth
+    const isAtStart = currentScroll === 0
+    const isAtEnd = currentScroll === maxScroll
+
+    const nearestIndexByOffset = offsets.findIndex((offset, i) => {
+      const nextOffset = offsets[i + 1] ?? Infinity
+
+      return (
+        pillsContainer.scrollLeft >= offset &&
+        pillsContainer.scrollLeft < nextOffset
+      )
+    })
+
+    setShowPreviousChevron(!isAtStart)
+    setShowNextChevron(!isAtEnd)
+    setIndex(nearestIndexByOffset)
+
+    console.log("[debug] nearestIndexByOffset", nearestIndexByOffset)
+
+    console.log(
+      "[debug] scroll",
+      pillsContainerRef.current.scrollWidth,
+      pillsContainerRef.current.clientWidth,
+      pillsContainerRef.current.scrollLeft,
+      maxScroll
+    )
+  }, [offsets])
+
+  useEffect(() => {
+    const pillsContainer = pillsContainerRef.current
+
+    if (!pillsContainer) {
+      return
+    }
+
+    pillsContainer.addEventListener("scroll", handleScroll, { passive: true })
+
+    return () => {
+      pillsContainer.removeEventListener("scroll", handleScroll)
+    }
+  }, [handleScroll])
+
+  const init = useCallback(() => {
+    if (!containerRef.current || !pillsContainerRef.current) {
+      return
+    }
+
+    const pillsContainer = pillsContainerRef.current
+    const maxScroll = pillsContainer.scrollWidth - pillsContainer.clientWidth
+    const values = pills.map(({ ref }) => {
+      return ref.current!.clientWidth
+    })
+
+    const compounded = compound(values)
+    const formatted = compounded.map(currentOffset =>
+      Math.abs(currentOffset - CHEVRON_CONTAINER_WIDTH)
+    )
+    const filtered = formatted.filter(value => maxScroll > value)
+
+    console.log("[debug] result", [0, ...filtered, maxScroll])
+
+    setOffsets([0, ...filtered, maxScroll])
+  }, [pills])
+
+  useEffect(() => {
+    init()
+  }, [init])
 
   return (
-    <Flex alignItems="center" bg="white">
-      <Flex position="absolute" left={0} zIndex={showPreviousChevron ? 0 : -1}>
-        <PreviousChevron onClick={scrollToLeft} left={2} />
-        <GradientBg placement="left" />
-      </Flex>
+    <Flex ref={containerRef as any} alignItems="center" bg="white">
+      {showPreviousChevron && (
+        <Flex
+          width={CHEVRON_CONTAINER_WIDTH}
+          height={CHEVRON_BTN_SIZE}
+          bg="red"
+          position="absolute"
+          left={0}
+        >
+          <PreviousChevron
+            onClick={event => {
+              event.preventDefault()
+              event.stopPropagation()
+              scrollToLeft()
+            }}
+            left={CHEVRON_BTN_OFFSET}
+          />
+          {/* <GradientBg placement="left" /> */}
+        </Flex>
+      )}
 
-      <PillsContainer
-        ref={pillsRef as any}
-        px={2}
-        py={2}
-        onScroll={handleScroll}
-      >
-        {PILLS_OPTIONS.map(pill => (
-          <Pill key={pill} mr={1}>
-            {pill}
-          </Pill>
+      <PillsContainer ref={pillsContainerRef as any} py={2}>
+        {pills.map(({ child, ref }) => (
+          <Box ref={ref as any}>{child}</Box>
         ))}
       </PillsContainer>
-      <Flex position="absolute" right={0} zIndex={showNextChevron ? 0 : -1}>
-        <NextChevron onClick={scrollToRight} right={2} />
-        <GradientBg placement="right" />
-      </Flex>
+
+      {showNextChevron && (
+        <Flex
+          width={CHEVRON_CONTAINER_WIDTH}
+          height={CHEVRON_BTN_SIZE}
+          bg="green"
+          position="absolute"
+          right={0}
+        >
+          <NextChevron
+            onClick={event => {
+              event.preventDefault()
+              event.stopPropagation()
+              scrollToRight()
+            }}
+            right={CHEVRON_BTN_OFFSET}
+          />
+          {/* <GradientBg placement="right" /> */}
+        </Flex>
+      )}
     </Flex>
   )
+}
+
+const useChildrenRefs = (children: ReactNode) => {
+  const cells = useMemo(() => {
+    const childrenAsArray = Children.toArray(children)
+    const validChildren = childrenAsArray.filter(isValidElement)
+    const childrenRefs = validChildren.map(child => ({
+      child,
+      ref: createRef<HTMLLIElement>(),
+    }))
+
+    return childrenRefs
+  }, [children])
+
+  return cells
 }
