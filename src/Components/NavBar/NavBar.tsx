986f1fd230c75cb52a56d5d030e63e0243e60592
Nikita Skalkin <nikskalkin@gmail.com>
2023-05-16 11:43:15 +0200

    Force enable ff for review app

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -80,9 +80,8 @@ export const NavBar: React.FC = track(
   const isLoggedIn = Boolean(user)
   const showNotificationCount = isLoggedIn && !showMobileMenu
 
-  const isSearchDropDownImprovementsEnabled = useFeatureFlag(
-    "fx-force-search-dropdown-improvements"
-  )
+  const isSearchDropDownImprovementsEnabled =
+    useFeatureFlag("fx-force-search-dropdown-improvements") || true // TODO: remove
 
   // Close mobile menu if dragging window from small size to desktop
   useEffect(() => {
@@ -194,8 +193,7 @@ export const NavBar: React.FC = track(
                 flex={1}
                 alignItems="center"
                 onFocus={() => {
-                  if (!isSearchDropDownImprovementsEnabled)
-                    setSearchFocused(true)
+                  setSearchFocused(true)
                 }}
                 // Update only on mobile
                 position={[
